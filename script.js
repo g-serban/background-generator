f5c837762a52c4b93aad3272e19fffa9ef9e3077
g-serban <56362373+g-serban@users.noreply.github.com>
2020-04-28 11:16:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-var css = document.querySelector("h3");
 var color0 = document.querySelector(".color0");
 var color1 = document.querySelector(".color1");
 var body = document.getElementById("gradient");
-
-// console.log(css);
-// console.log(color1);
-// console.log(color2);
-// console.log(body);
 
 // color0.addEventListener("input", function() {
 //     body.style.background = "linear-gradient(to right, " + color0.value + ", " + color1.value + ")";
@@ -19,8 +13,7 @@ var body = document.getElementById("gradient");
 
 function setGradient() {
     body.style.background = "linear-gradient(to right, " + color0.value + ", " + color1.value + ")";
-    css.textContent = body.style.background + ";";
-}
+ }
 
 color0.addEventListener("input", setGradient);
 color1.addEventListener("input", setGradient);
